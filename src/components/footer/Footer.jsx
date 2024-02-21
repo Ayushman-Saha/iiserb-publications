2e66b225280c32be4a08e1e1dd9e4e0aa5c2ff1b
Ayushman Saha <ayushman.2004.as@gmail.com>
2024-02-21 23:12:36 +0530

    Removed developed by

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,9 +7,9 @@ function Footer() {
         <div className="inline-flex items-center">
           <img src={IISERBLogo} className=""/>
         </div>
-        <div className="mt-4 md:mt-0">
+        {/* <div className="mt-4 md:mt-0">
           <p className="text-sm font-medium text-gray-500">Developed by <a className="underline" href="https://www.linkedin.com/in/ayushman-saha/" target="_blank">Ayushman Saha</a> </p>
-        </div>
+        </div> */}
         <div className="mt-4 md:mt-0">
           <p className="text-sm font-medium text-gray-500">© {time.getFullYear()} IISER Bhopal. All rights reserved.</p>
         </div>
